4981455b7e7f82e3cb8e60c72d1cd5ee6cd6dfe7
chrysanthemum <asingiser@gmail.com>
2024-04-02 02:42:15 -0400

    update Circle

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Circle.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Circle.java
@@ -1,2 +1,7 @@
-package org.launchcode;public class Circle {
+package org.launchcode;
+
+public class Circle {
+    public static Double getArea(Double radius) {
+        return Math.PI * (radius * radius);
+    }
 }
